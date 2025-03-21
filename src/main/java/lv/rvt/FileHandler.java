846f30803212b0491220c0fc48c9313632cde6a9
Marta Aleksa Valaine <166013386+23DP3MVala@users.noreply.github.com>
2025-03-21 08:43:38 +0000

    Pievienota faila saglabāšanas un ielādes klase

---
+++ src/main/java/lv/rvt/FileHandler.java
@@ -0,0 +1,29 @@
+package lv.rvt;
+import com.fasterxml.jackson.*;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.*;
+import java.util.*;
+
+public class FileHandler {
+    private static final String path = "data/notes.json";
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    public static void Saglabat(List<Piezime> piezimes) {
+        try {
+            objectMapper.writeValue(new File(path), piezimes);
+        } catch (IOException e) {
+            System.out.println("Kļūda piezīmju saglabāšanā: " + e.getMessage());
+        }
+    }
+
+    public static List<Piezime> Ieladet() {
+        try {
+            return objectMapper.readValue(new File(path), new TypeReference<List<Piezime>>() {});
+        } catch (IOException e) {
+            System.out.println("Iepriekšējās piezīmes netika atrastas. Izveidots jauns fails.");
+            return new java.util.ArrayList<>();
+        }
+    }
+}
